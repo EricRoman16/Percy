78290bcf59f8e775e6d2f6d80a8741e4e20f530a
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-04-13 10:17:43 -0700

    Added some brown tiles and a circle for player, added player movement

--- Assets/EricRoman/Scripts/PlayerScript.cs
+++ Assets/EricRoman/Scripts/PlayerScript.cs
@@ -13,6 +13,16 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D))
+        {
+            transform.position += new Vector3(Mathf.Floor(Input.GetAxisRaw("Horizontal")), 0f, 0f);
+            //Vector3.Lerp(transform.position, transform.position + new Vector3(-1f, 0f, 0f), .1f);
+        }
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.S))
+        {
+            transform.position += new Vector3(0f, Mathf.Floor(Input.GetAxisRaw("Vertical")), 0f);
+        }
     }
+
+    
 }

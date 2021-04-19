7568302c94c3288cb06fc0c895883e35b546e79f
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-04-19 12:50:07 -0700

    added abilities script

---
+++ Assets/EricRoman/Scripts/PlayerAbilities.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAbilities : MonoBehaviour
+{
+    public bool hasSpeedUpgrade = false;
+    public bool hasSmokeUpgrade = false;
+    public bool hasHealthUpgrade1 = false;
+    public bool hasHealthUpgrade2 = false;
+    public bool hasHealthUpgrade3 = false;
+}

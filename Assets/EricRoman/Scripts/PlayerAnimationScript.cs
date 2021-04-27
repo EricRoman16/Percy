ca31cb6784f89a1ce077c3969b0beea819293af2
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-04-26 18:37:31 -0700

    added the player sprites

---
+++ Assets/EricRoman/Scripts/PlayerAnimationScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationScript : MonoBehaviour
+{
+    public Sprite Up;
+    public Sprite Down;
+    public Sprite Right;
+    public Sprite Left;
+
+    public SpriteRenderer SR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SR.sprite = Down;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            SR.sprite = Left;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            SR.sprite = Down;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SR.sprite = Right;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            SR.sprite = Up;
+        }
+    }
+}

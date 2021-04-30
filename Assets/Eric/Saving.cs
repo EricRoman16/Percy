efeac0473bf3c3f313724f56088a4a5bf322577b
Eric <ermorgan@protonmail.com>
2021-04-29 18:12:38 -0700

    crude saving and loading

---
+++ Assets/Eric/Saving.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class Saving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/save.dat");
+        UpgradeSave data = new UpgradeSave();
+        data.smoke = false;
+        data.speed = false;
+        data.health = 0;
+        bf.Serialize(file, data);
+        Debug.Log("Save Success");
+    }
+    [Serializable]
+    class UpgradeSave
+    {
+        public bool smoke;
+        public bool speed;
+        public byte health;
+    }
+
+    public void Load()
+    {
+        try{
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/save.dat", FileMode.Open);
+            UpgradeSave data = (UpgradeSave)bf.Deserialize(file);
+            file.Close();
+            Debug.Log("Load Success");
+        }
+        catch (Exception error)
+        {
+            Debug.Log(error);
+        }
+    }
+}

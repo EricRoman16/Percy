acf420a3e40d198c422da3a7ba7c539b03d6386f
EricRoman16 <1707234@mukilteo.wednet.edu>
2021-05-11 09:26:28 -0700

    fixed player movement

--- Assets/EricRoman/Scripts/Player/PlayerScript.cs
+++ Assets/EricRoman/Scripts/Player/PlayerScript.cs
@@ -14,20 +14,20 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) && !west)
+        if (Input.GetKeyDown(KeyCode.A) && west)
         {
             transform.position += new Vector3(Mathf.Floor(Input.GetAxisRaw("Horizontal")), 0f, 0f);
             //Vector3.Lerp(transform.position, transform.position + new Vector3(-1f, 0f, 0f), .1f);
         }
-        else if (Input.GetKeyDown(KeyCode.D) && !east)
+        else if (Input.GetKeyDown(KeyCode.D) && east)
         {
             transform.position += new Vector3(Mathf.Floor(Input.GetAxisRaw("Horizontal")), 0f, 0f);
         }
-        else if (Input.GetKeyDown(KeyCode.W) && !north)
+        else if (Input.GetKeyDown(KeyCode.W) && north)
         {
             transform.position += new Vector3(0f, Mathf.Floor(Input.GetAxisRaw("Vertical")), 0f);
         }
-        else if (Input.GetKeyDown(KeyCode.S) && !south)
+        else if (Input.GetKeyDown(KeyCode.S) && south)
         {
             transform.position += new Vector3(0f, Mathf.Floor(Input.GetAxisRaw("Vertical")), 0f);
         }

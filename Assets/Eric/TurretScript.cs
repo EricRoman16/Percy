f046b25ef68e0ab91817be4dd988d4f4c12bce45
Eric <ermorgan@protonmail.com>
2021-05-13 17:50:36 -0700

    Turrets 2

--- Assets/Eric/TurretScript.cs
+++ Assets/Eric/TurretScript.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class TurretScript : MonoBehaviour
 {
+    public float DetectRadius;
+    public GameObject player;
+    public float distX;
+    public float distY;
+    public float angle;
+    public float dangle;
+    public bool detected;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +22,21 @@ public class TurretScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Determine Distance
+        distX = Mathf.Abs(player.transform.position.x - transform.position.x);
+        distY = Mathf.Abs(player.transform.position.y - transform.position.y);
+        if (distX < DetectRadius && distY < DetectRadius)
+        {
+            detected = true;
+        }
+        else
+        {
+            detected = false;
+        }
         //Determine Angle
-            //Determine Sprite Frame
+        angle = Mathf.Atan2(distY, distX);
+        dangle = Mathf.Rad2Deg * angle;
+        //Determine Sprite Frame
         //Shoot bullet
         //
     }

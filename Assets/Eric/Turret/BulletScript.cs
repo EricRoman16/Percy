5ec59c740dd797c66df17bb2e5f51e7c4ee8ffc2
Eric <ermorgan@protonmail.com>
2021-05-25 18:11:46 -0700

    Victory Button moves, Turrets are ready for placement

--- Assets/Eric/Turret/BulletScript.cs
+++ Assets/Eric/Turret/BulletScript.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject == player)
+        {
+            //reduce player health
+        }
+        Object.Destroy(gameObject);  //destroys itself
     }
 }

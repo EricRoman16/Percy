407f8fc1960e895e318cd815bed2ff246b660e72
Eric <ermorgan@protonmail.com>
2021-05-26 13:52:55 -0700

    volume

---
+++ Assets/Scenes/VolumePrefs.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumePrefs : MonoBehaviour
+{
+    public Slider vol;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vol.value = PlayerPrefs.GetFloat("volume", 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerPrefs.SetFloat("volume", vol.value);
+    }
+}
